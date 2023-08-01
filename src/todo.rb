453e6e03395b2e2ae20296b748dee195653f2303
SKTM307 <sktm307@proton.me>
2023-08-01 21:59:13 +0200

    secondary commit

--- src/todo.rb
+++ src/todo.rb
@@ -4,10 +4,13 @@
 
 require 'json'
 
+### SETTINGS ###
+
 TODO_FILE="#{ENV["HOME"]}/.local/share/todo.json"
 CHCK_SYMBOL='X'
 UCHCK_SYMBOL='O'
 COLORS=true
+DEFAULT='list'
 
 if COLORS
   COL_NUM  ="\e[01;36m"
@@ -31,26 +34,25 @@ else
   COL_RESET=''
 end
 
-class Task
-  @name=''
-  @desc=''
-  @chck=false
-  @tasks=[]
+### CLASSES ###
 
+class Task
   def initialize(list={})
-    @name=list['name']
+    name=list['name']
+    @name=(name.is_a? String and not name.empty?) ? name : 'Unnamed task'
     @desc=list['desc']
     @chck=list['chck']
     @tasks=[]
-    if list['tasks'].is_a?Array
-      for x in list['tasks']
-        @tasks+=[Task.new(x)]
+    tasks=list['tasks']
+    if tasks.is_a? Array
+      for task in tasks
+        @tasks+=[Task.new(task)]
       end
     end
   end
 
-  def [](x)
-    if x.is_a?Numeric
+  def [](x) # TODO: prettify
+    if x.is_a? Numeric
       if x>=0 and x<@tasks.count() 
         return @tasks[x]
       else
@@ -62,9 +64,9 @@ class Task
       if x==[]
         return self
       end
-      x_pop=x.pop
-      if x_pop>=0 and x_pop<@tasks.count() 
-        task=@tasks[x_pop]
+      i=x.shift
+      if i>=0 and i<@tasks.count() 
+        task=@tasks[i]
       else
         STDERR.print("invalid index\n")
         exit 1
@@ -102,9 +104,8 @@ class Task
         @tasks.delete_at x[0]
         return self
       end
-      i=x.reverse.pop
-      x.reverse
-      task=@tasks[x.pop]
+      i=x.pop
+      task=@tasks[x.shift]
       if task==nil
         STDERR.print("invalid index\n")
         exit 1
@@ -114,16 +115,16 @@ class Task
     self
   end
 
-  def getList
+  def getHash
     {
-      'name'=>@name,
-      'desc'=>@desc,
-      'chck'=>@chck,
-      'tasks'=>@tasks.map{ |x| x.getList }
+      'name'  => @name,
+      'desc'  => @desc,
+      'chck'  => @chck,
+      'tasks' => @tasks.map{ |x| x.getHash },
     }
   end
 
-  def print(i=nil,depth=0)
+  def print(i=nil,depth=0) # TODO: prettify
       n=@name
       d=@desc
       if @chck
@@ -152,7 +153,7 @@ class Task
   end
 
   def getTask(idx)
-    task=@tasks[idx.pop]
+    task=@tasks[idx.shift]
     if task==nil
       STDERR.print("invalid task index\n")
       exit 1
@@ -172,98 +173,151 @@ class Task
   end
 end
 
-def getIdx(x)
-  x.split('.').map!{ |x| x.to_i - 1 }.reverse
-end
-
-def readTasks
-  begin
-  file=open(TODO_FILE,'r')
-  begin
-    tasks=JSON.parse file.read
-  rescue JSON::ParserError
-    tasks={}
+class Main
+  def initialize
+    @commands={
+      'get'    => {method: method(:get)     ,w: false ,help: 'shows item at [index]'},
+      'getsub' => {method: method(:getsub)  ,w: false ,help: 'shows subitems of [index]'},
+      'getall' => {method: method(:getall)  ,w: false ,help: 'shows item and it\'s subitems at [index]'},
+      'list'   => {method: method(:list)    ,w: false ,help: 'lists all items'},
+      'add'    => {method: method(:add)     ,w: true  ,help: 'adds item with [name] and [description]'},
+      'addsub' => {method: method(:addsub)  ,w: true  ,help: 'adds item under [index] with [name] and [description]'},
+      'check'  => {method: method(:check)   ,w: true  ,help: 'checks item at [index]'},
+      'uncheck'=> {method: method(:uncheck) ,w: true  ,help: 'unchecks item at [index]'},
+      'remove' => {method: method(:remove)  ,w: true  ,help: 'removes item at [index]'},
+      'help'   => {method: method(:help)    ,w: false ,help: 'shows this help menu'},
+    }
+    command=getArg default: DEFAULT
+    @tasks=Task.new(readTasks())
+    method=@commands[command][:method]
+    if method.is_a? NilClass
+      STDERR.print("invalid command\n")
+      exit 1
+    end
+    method.call
+    if @commands[command][:w]
+      writeTasks @tasks.getHash
+    end
   end
-  file.close
-  rescue Errno::ENOENT
-    tasks={}
+
+  def getArg(prompt=nil,type: :normal,default: nil, required: false)
+    if not ARGV.empty?
+      x=ARGV.shift
+    elsif not default.is_a?NilClass
+      return default
+    else
+      x=''
+      begin
+        if prompt.is_a?String
+          print "#{prompt}: "
+        end
+        x=STDIN.readline.chomp
+      end while x.empty? and required
+    end
+    case type
+    when :normal
+      return x
+    when :index
+      return getIdx x
+    end
   end
-  tasks
-end
 
-def writeTasks tasks
-  file=open(TODO_FILE,'w')
-  file.print(JSON.generate(tasks))
-  file.close
-end
-
-def popArg(args,prompt='')
-  if not x=(args.pop) then
-    print prompt
-    x=STDIN.readline().chomp()
+  def getIdx(x)
+    x.split('.').map!{ |x| x.to_i - 1 }
   end
-  return x,args
+
+  def readTasks
+    begin
+    file=open(TODO_FILE,'r')
+    begin
+      tasks=JSON.parse file.read
+    rescue JSON::ParserError
+      tasks={}
+    end
+    file.close
+    rescue Errno::ENOENT
+      tasks={}
+    end
+    tasks
+  end
+
+  def writeTasks tasks
+    file=open(TODO_FILE,'w')
+    file.print(JSON.generate(tasks))
+    file.close
+  end
+  
+  ### COMMANDS ###
+
+  def get
+    idx=getArg "index",type: :index
+    task=@tasks[idx]
+    task.print
+    print "subtasks #{task.subTasks}\n"
+  end
+
+  def getsub
+    idx=getArg "index",type: :index
+    task=@tasks[idx]
+    task.listSubTasks
+  end
+
+  def getall
+    idx=getArg "index",type: :index
+    task=@tasks[idx]
+    task.print
+    task.listSubTasks 1
+  end
+
+  def list
+    @tasks.listSubTasks
+  end
+
+  def add
+    name=getArg "name",required: true
+    desc=getArg "description"
+    @tasks+={"name"=>name,"desc"=>desc,"tasks"=>[],"chck"=>false}
+    writeTasks @tasks.getHash
+  end
+
+  def addsub
+    idx=getArg "index",type: :index
+    name=getArg "name",required: true
+    desc=getArg "description"
+    @tasks[idx]+={"name"=>name,"desc"=>desc,"tasks"=>[],"chck"=>false}
+  end
+
+  def check
+    idx=getArg "index",type: :index
+    @tasks[idx].check
+  end
+
+  def uncheck
+    idx=getArg "index",type: :index
+    @tasks[idx].uncheck
+  end
+
+  def remove
+    idx=getArg "index",type: :index
+    @tasks-=idx
+  end
+
+  def help
+    message=\
+    "Usage: #{$0} [command] [arguments]\n"\
+    "a simple commandline todo app written in ruby\n"\
+    "   \n"
+    for command in @commands.keys
+      message+=sprintf "  %7s - %s\n",command,@commands[command][:help] # TODO: change to dynamic length
+    end
+    if not DEFAULT.is_a? NilClass
+      message+="\nif no command is supplied \"#{DEFAULT}\" command will be used\n"
+    end
+    print message
+  end
 end
 
-# main
+### MAIN ###
 
-if ARGV!=[]
-  args=ARGV.reverse
-  ARGV.clear
-else
-  args=['list']
-end
-
-x,args=popArg args
-tasks=Task.new(readTasks())
-case x
-when 'get'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  task=tasks[idx]
-  task.print
-  print "subtasks #{task.subTasks}\n"
-when 'getsub'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  task=tasks[idx]
-  task.listSubTasks
-when 'getall'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  task=tasks[idx]
-  task.print
-  task.listSubTasks 1
-when 'list'
-  tasks.listSubTasks
-when 'add'
-  name,args=popArg args,"name: "
-  desc,args=popArg args,"description: "
-  tasks+={"name"=>name,"desc"=>desc,"tasks"=>[],"chck"=>false}
-  writeTasks tasks.getList
-when 'addsub'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  name,args=popArg args,"name: "
-  desc,args=popArg args,"description: "
-  tasks[idx]+={"name"=>name,"desc"=>desc,"tasks"=>[],"chck"=>false}
-  writeTasks tasks.getList
-when 'check'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  tasks[idx].check
-  writeTasks tasks.getList
-when 'uncheck'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  tasks[idx].uncheck
-  writeTasks tasks.getList
-when 'remove'
-  str_idx,args=popArg args,"index: "
-  idx=getIdx str_idx
-  tasks-=idx
-  writeTasks tasks.getList
-else
-  STDERR.print("invalid command\n")
-end
-
+main=Main.new
+exit 0
